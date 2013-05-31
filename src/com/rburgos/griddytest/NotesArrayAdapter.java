f89138dd4d8083530c66d1f0de35bdefe85afd8e
rburgosnavas <roby0b@gmail.com>
2013-05-31 01:01:24 -0700

    New custom ArrayAdapter class. Now displays an icon next to each entry.

---
+++ src/com/rburgos/griddytest/NotesArrayAdapter.java
@@ -0,0 +1,31 @@
+package com.rburgos.griddytest;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class NotesArrayAdapter extends ArrayAdapter<Note>
+{
+	private List<Note> notes;
+
+	public NotesArrayAdapter(Context context, List<Note> notes)
+	{
+		super(context, R.layout.notes_list_custom, R.id.notes_list_title, notes);
+		this.notes = notes;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent)
+	{
+		View row = super.getView(position, convertView, parent);
+		ImageView icon = (ImageView) row.findViewById(R.id.list_icon);
+		TextView title = (TextView) row.findViewById(R.id.notes_list_title);
+		title.setText(notes.get(position).getTitle());
+		return row;
+	}
+}
